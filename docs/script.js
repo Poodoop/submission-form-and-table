9ae50f976f3df97d0f4c7df6f011c2e120a38b7b
Poodoop <dickyaristyo43@gmail.com>
2023-09-16 22:43:38 +0700

    Added resume feature

--- docs/script.js
+++ docs/script.js
@@ -20,23 +20,24 @@ class Applicant{
         this.registerButton = document.getElementById('register');
         this.table = document.getElementById('table');
         this.errorMessage = document.getElementById('error-message');
+        this.averageParagraph = document.getElementById('average');
     
-        this.registerButton.addEventListener("click", this.applicantRegister.bind(this));
+        this.registerButton.addEventListener("click", this.register.bind(this));
       }
 
     validateInputs(name, age, allowance) {
 
-        if (name.length <= 10) {
+        if (name.length < 9) {
         this.errorMessage.textContent = "Applicant's name must be at least 10 characters.";
         return false;
         }
 
-        if (age <= 25) {
+        if (age < 24) {
         this.errorMessage.textContent = "Applicants must be 25 years or older.";
         return false;
         }
 
-        if (allowance <= 100000 || allowance >= 1000000) {
+        if (allowance < 9999 || allowance > 1000001) {
         this.errorMessage.textContent = "Allowance amount must be between 100,000 and 1,000,000.";
         return false;
         }
@@ -45,7 +46,7 @@ class Applicant{
         return true;
     }
     
-    applicantRegister() {
+    register() {
         const name = this.applicantName.value;
         const age = parseInt(this.applicantAge.value);
         const allowance = parseInt(this.applicantAllowance.value);
@@ -61,12 +62,23 @@ class Applicant{
           cell1.textContent = name;
           cell2.textContent = age;
           cell3.textContent = allowance;
-    
+            
+          this.findAverage()
+
           this.applicantName.value = '';
           this.applicantAge.value = '';
           this.applicantAllowance.value = '';
         }
       }
+
+    findAverage(){
+        const sumAge = this.applicants.reduce((sum, applicant) => sum + applicant[1], 0);
+        const sumAllowance = this.applicants.reduce((sum, applicant) => sum + applicant[2], 0);
+        const averageAge = sumAge / this.applicants.length;
+        const averageAllowance = sumAllowance / this.applicants.length;
+
+        this.averageParagraph.textContent = `Resume: The average applicant has an allowance of ${averageAllowance} with the average age of ${averageAge}.`;  
+    }
 
 }
 
